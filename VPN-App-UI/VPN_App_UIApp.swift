3e5f61ddea10d46aa6ebdf2cdeb8f3d5fb3d5a81
Md Jubel Hossain <69891127+JubelHossaain@users.noreply.github.com>
2021-11-25 23:29:51 +0600

    Initial Commit

---
+++ VPN-App-UI/VPN_App_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPN_App_UIApp.swift
+//  VPN-App-UI
+//
+//  Created by Md Jubel Hossain on 25/11/21.
+//
+
+import SwiftUI
+
+@main
+struct VPN_App_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
